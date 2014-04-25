904ba60e31262103f43978c163fea8f19b28fcad
hinasakazaki <hinasakazaki@gmail.com>
2014-04-24 19:59:02 -0700

    content on front pagee

--- js/hinasakazaki.js
+++ js/hinasakazaki.js
@@ -15,6 +15,13 @@ $(document).ready(function() {
     //      $(‘#header’).css(‘background’, ‘#d4c5d4’).fadeOut(500).fadeIn(500); 
     // });
 
+    $('#hina-img').hover(function(){
+        $('#hina-img').attr('src', 'img/hina.gif');
+        $('#hina-img').attr('height', '300');
+        $('#hina-img').attr('width', '230');
+        $('#hina-img').text("SWAG!");
+    });
+
     $(window).scroll(function() {
         if($(document).scrollTop() > 107) {
             $('#header').slideUp();
