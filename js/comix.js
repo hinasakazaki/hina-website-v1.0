ff25c159b052913dccc4d40e0c38b6167c7cd7a8
hinasakazaki <hinasakazaki@gmail.com>
2014-05-21 16:07:43 -0700

    little changes and trying to change faviconnn

--- js/comix.js
+++ js/comix.js
@@ -1,41 +1,19 @@
 $(document).ready(function() {
 
-    currPage= 2;
-    mostRecentPage = 2;
-    
-    function getPrev(currPage) {
-        return currPage-1; 
+
+    $('.frame1').click(function() {
+        /* Actions on thumbnail */
+        $('.thumbnail').removeClass('active');
+        $(this).addClass('active');
+        /* Actions on large image */
+        var src = $(this).find('.thumbnail-large').text();
+        $('#large-img').find('img').attr('src', src);
     });
-
-    function getNext(currPage) {
-        if (currPage = mostRecentPage) {
-            $('#prev').hide();
-        }
-        else {        
-            return currPage+1;
-        }
-    });
-
-    function getRandom(currPage) {
-        return (currPage/2);
-    });
-
-    $('.button').click(function() {
-        alert("Your book is overdue");
-       //$('#frame').find('img').attr('src', 'img/comix/' + getPrev(currPage) +'.png');
-    });
-
-    $('#random').click(function() {
-        $('#frame').find('img').attr('src', 'img/comix/' + getRandom(currPage) +'.png');
-    });
-
-    $('#next').click(function() {
-        $('#frame').find('img').attr('src', 'img/comix/' + getNext(currPage) +'.png');
-    });
-
-
     /* Defaults */
-    $('#frame:first').addClass('active');
-    $('#frame').find('img').attr('src', 'img/comix/' + currPage + '.png');
+    $('.thumbnail:first').addClass('active');
+    $('#large-img').find('img').attr('src', 'https://farm8.staticflickr.com/7327/13484724834_12c708d8c5.jpg');
 
+
+
+    
 });
