58cfe7405474f3f5f4606ce9cca23d2689567663
Hina Sakazaki <hinasakazaki@gmail.com>
2015-01-29 15:45:53 -0800

    Done with blogs, need comics and projects and stuff

---
+++ hina_django/blog/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import patterns, url
+
+from blog import views
+
+urlpatterns = patterns('',
+    # ex: /polls/
+    url(r'^$', views.index, name='index'),
+    # ex: /polls/5/
+    url(r'^(?P<post_id>\d+)/$', views.detail, name='detail'),
+    url(r'^archive/', views.archive, name='archive'),
+    
+)
